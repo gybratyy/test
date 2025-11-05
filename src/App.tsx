2a0f466f103c88263b4e05e25268e21d629a2d66
gybratyy <220001@astanait.edu.kz>
2025-11-05 15:59:57 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import 'leaflet/dist/leaflet.css';
+import './App.css'
+
+function App() {
+
+  return (
+      <>
+          <h1 className="text-3xl font-bold underline text-black">
+              Hello world!
+          </h1>
+      </>
+  )
+}
+
+export default App
